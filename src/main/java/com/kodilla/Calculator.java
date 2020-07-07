553d96b1695c77a687e01bcbf28afde29e3e6a76
KrzysiekOkonski <krzysztof.okonski@interia.pl>
2020-07-07 11:38:33 +0200

    Second commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,10 @@
+package com.kodilla;
+
+public class Calculator {
+    public int adding(int a, int b){
+        return a+b;
+    }
+    public int subtracting(int a, int b){
+        return a-b;
+    }
+}
---
+++ src/main/java/com/kodilla/CalculatorApplication.java
@@ -0,0 +1,18 @@
+package com.kodilla;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+
+
+		Calculator calculator = new Calculator();
+		System.out.println(calculator.subtracting(10,5));
+	}
+}
+
+
+
